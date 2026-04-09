77d380ad048039a700815aab723c858c9ab27a7a
tyron <wishsmp1002@gmail.com>
2026-04-10 06:51:34 +1000

    Basic Java Syntax Information

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,23 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello Magnet108!");
+        /* JAVA BASIC SYNTAX */
+
+        /* Variables */
+
+        // datatype name_of_variable // Declaration
+        // OR
+        // datatype name_of_variable = value_of_variable;
+        // Initialization (Declaration + Assignment of Value)
+
+        int points; // Declaration
+        points = 100; // Assignment
+        int x = -201; // Initialization (Declaration + Assignment)
+
+        // Every line of code ends in a semicolon
+        // int y = 42 <-- will error
+
+
 
     }
 }
