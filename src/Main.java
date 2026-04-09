47e7cad739f1156e66a281ccea88520b0403722e
tyron <wishsmp1002@gmail.com>
2026-04-10 07:06:20 +1000

    Java Primary Datatypes with information, variables, comments and multiline comments

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,13 @@
 public class Main {
     public static void main(String[] args) {
 
-        /* JAVA BASIC SYNTAX */
+        /* DATA TYPES & VARIABLES */
+
+        // <-- comment
+
+        /*
+        Multiline Comment
+         */
 
         /* Variables */
 
@@ -11,14 +17,30 @@ public class Main {
         // datatype name_of_variable = value_of_variable;
         // Initialization (Declaration + Assignment of Value)
 
-        int points; // Declaration
-        points = 100; // Assignment
-        int x = -201; // Initialization (Declaration + Assignment)
+        /* DATA TYPES */
 
-        // Every line of code ends in a semicolon
-        // int y = 42 <-- will error
+        // Integers (Store whole numbers including negative numbers)
+        // Ints go from -2147483648 to 2147483647
+        int points;
+        points = 100;
+        int x = -69;
 
+        // Floating Point Numbers (float & double)
+        float pi = 3.14159265F;
+        double e = 2.71828128;
 
+        float accountValue = 20.20f; // has precision errors GOOD FOR MINECRAFT YUMMY YUMM ! :D
+
+        // For real numbers, Precision can be an issue
+        // Scientific notation: 314159265 * 10^-8
+
+        // Booleans (True or False)
+        boolean pumpkinIsAPumpkin = true;
+        boolean pumpkinIsStraight = false;
+
+        // Chars & Strings
+        char grade = 'B';
+        String name = "PumpkinDev";
 
     }
 }
