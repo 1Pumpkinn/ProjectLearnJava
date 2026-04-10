186b510578904834ad4dac8e87f6da96e9a520af
tyron <wishsmp1002@gmail.com>
2026-04-11 04:13:03 +1000

    Math Methods * Addition * Subtraction * Multiplication * Division * Remainder (MODULO) * Math Methods Rounding, Rounding up, Rounding Down, and power.

--- src/Main.java
+++ src/Main.java
@@ -3,32 +3,78 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-       /* INPUT & OUTPUT */
+      /* INTEGERS & MATH */
+        // Equals is the assignment operator
+        // Plus is the addition operator
 
-        /* OUTPUT */
-        // You usually output Strings
+        int a = 100;
+        int b = 200;
 
-        System.out.println("Hello World!");
-        System.out.println("Element Smp 💥🔫");
+        /* ADDITION */
+        // a + b = sum;
+        // does not work because the assignment operator
+        // assigns the value from the right to the left
 
-        // Values
-        int health = 42;
-        System.out.println(health);
+        int sum = a + b;
+        System.out.println("Sum: " + sum);
 
-        // Values with Strings
-        int grade = 85;
-        System.out.println("Your grade is: " + grade);
+        sum = 50;
+        System.out.println("Sum: " + sum); // Overrides sum
 
-        /* INPUT */
-        // we make a variable data type of Scanner
-        // this allows us to read something in
-        Scanner scanner = new Scanner(System.in);
-        int number = scanner.nextInt(); // HAS TO BE A NUMBER
-        System.out.println("You just typed " + number);
-        // outputs number what is the users value that they input into the console.
+        /* SUBTRACTION */
 
-        String input = scanner.next(); // gets everything up until the next space.
-        System.out.println("You just typed (String) " + input);
+        int sub = a - b;
+        System.out.println("Subtraction: " + sub);
+
+        /* MULTIPLICATION */
+
+        int multi = a * b;
+        System.out.println("Multiplication: " + multi);
+
+        /* DIVISION */
+
+        int division = a / b;
+        System.out.println("Division: " + division);
+        // Cannot have 0.5 because we are using a int
+
+        double x = 100;
+        double y = 200;
+        double doubleDivsion =  x / y;
+        System.out.println("DoubleDivision: " + doubleDivsion);
+        // Outputs 0.5 because we are using a double
+
+        // Never divide by 0
+        //division = b / 0;
+        //System.out.println("Division: " + division);
+        // Outputs error
+        // Exception in thread "main" java.lang.ArithmeticException: / by zero
+
+
+        /* REMAINDER (MODULO) */
+
+        int remainder = 100 % 2; // = 0 => Even Number | = 1 => Uneven Number!
+        // This pretty much does is take 100 divides it by 2 and then looks for anything remains
+        // 2 fits into 100 150 times
+        System.out.println("Remaining from 100 / 2: " + remainder);
+
+        remainder = 100 % 3;
+        System.out.println("Remaining from 100 / 3: " + remainder);
+        // remains 1
+
+        remainder = 100 % 4;
+        System.out.println("Remaining from 100 / 4: " + remainder);
+
+
+        /* MATH METHODS */
+
+        System.out.println("Rounding " + Math.round(6.4));        // Output: 6
+        System.out.println("Rounding Down " + Math.floor(6.4));   // Output: 6 // returns a double
+        System.out.println("Rounding Down " + Math.floor(500.2f));   // Output: 6 // returns a float
+        System.out.println("Rounding Up " + Math.ceil(6.4));      // Output: 7 // returns a double
+        System.out.println("Exponent " + Math.pow(2, 3));         // Output: 2^3 = 8 returns a double
+
+        // More "Math Methods"
+        // Math. <---
 
 
 
@@ -36,4 +82,3 @@ public class Main {
 
     }
 }
-
