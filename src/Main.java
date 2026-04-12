3e0cf3aa43024f489d9677fcbfb790b8d6bd26a1
tyron <wishsmp1002@gmail.com>
2026-04-13 09:17:50 +1000

    For a while loops as well as breaking and continuing
    continue;
    break;

--- src/Main.java
+++ src/Main.java
@@ -2,41 +2,75 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        /* (BASIC) Casting */
-        // Turning one Data Type into another
+        /* LOOPS (FOR & WHILE */
+        // Doing things Multiple times (easily)
+        // Doing things Multiple times (easily)
 
+        // Output all numbers from 0 to 99
+        System.out.println(0);
+        System.out.println(1);
+        System.out.println(2);
+        System.out.println(3);
+        System.out.println(4);
+
+        // Now output numbers between 1000 and 2000 and only leap years!
+        System.out.println(1000);
+        System.out.println(1004);
+        System.out.println(1008);
+
+        /* FOR LOOP */
+        // for(Initialization; Condition; Update) if condition is false the loop stops
+        // Example 0 to 99
+        for(int i = 0; i < 100; i++) { // if "i" is smaller than 100 keep adding until you get to 99
+
+            if(i == 56 || i == 57 || i == 58) {
+                continue;
+            }
+            System.out.println(i);
+        }
+
+        int countTo = 100;
+        for(int i = 0; i < countTo; i++) {
+
+            System.out.println(i);
+        }
+
+        // Example: Leap years
+        for(int i = 1000; i <= 2000; i++) {
+
+            if(i == 1980) {
+                break;
+            }
+
+            // If the year is divisible by 4 AND (it's NOT divisible by 100 OR it is divisible by 400)
+            if(i % 4 == 0 && (i % 100 != 0 || i % 400 == 0)) {
+                System.out.println(i);
+            }
+        }
+
+        /* WHILE LOOP */
         int health = 10;
-        float damage = 1.5f;
+        while (health > 0) { // Infinite loop
+            System.out.println("The Player is still Alive with " + health + " HP");
+            health--;
+        }
 
-        // int result = health - damage; // 10 - 1.5f = 8.5f
-        float resultFloat = health - damage; // this works because it's a float!
+        System.out.println("-------------");
 
-        System.out.println("Result: " + resultFloat); // 8.5f
+        // CAUTION for While Loops
+        // They can cause: INFINITE LOOPS!
+        health = 10;
+        while (health > 0) {
+            if(health == 6) {
+                continue; // skip the rest of the loop
+            }
+            if(health == 2) {
+                break; // exit and break out of the loop
+            }
+            System.out.println("The Player is still Alive with " + health + " HP");
+            health--;
 
-        int resultInt = health - (int) damage; // explicit "Cast" damage from float to int
-        System.out.println("Result: " + resultInt);
-
-        resultFloat = (float) health - damage;
-        System.out.println(resultFloat); // 10 -> 10.0
-
-        resultInt = (int) ((float) health - damage); // 8.5 to int --> 8
-        System.out.println(resultInt);
-
-        // health at 10 and damage at 9.99 -> 9.99 -> 9 everything gets cut off from the decimal
-
-       // resultInt = (boolean) health; Not convertible
-
-        String points = "100";
-        String pi = "3.14159265";
-
-        int result = Integer.parseInt(points); // String to Integer
-        System.out.println("Result: " + result);
-
-        double resultPi = Double.parseDouble(pi);
-        System.out.println("Result Pi: " + resultPi);
-
-
-
-
+            }
+        }
     }
-}
+
