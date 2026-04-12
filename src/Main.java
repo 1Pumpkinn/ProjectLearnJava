21116cf4ab107dcd09fa15af19853a89ac1ff7de
tyron <wishsmp1002@gmail.com>
2026-04-13 06:52:44 +1000

    Ternary Operator
    Boolean ? true return : false return;

--- src/Main.java
+++ src/Main.java
@@ -2,68 +2,25 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        /* TERNARY OPERATOR */
+        // A very "compact" way to do if-else
 
+        int health = 0;
+        String status = "";
+        System.out.println("What is your Status? " + status);
 
-        // Use cases for operation operator +, - * /
-        // or If statements if easier
-        // Try clean code instead of messy
-
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Input the Math Operator +, -, *, /");
-        String operator = scanner.next();
-        System.out.println("Input the first number");
-        int num1 = scanner.nextInt();
-        System.out.println("Input the second number");
-        int num2 = scanner.nextInt();
-
-        switch(operator) {
-            case "+": System.out.println(num1 + num2); break;
-            case "-": System.out.println(num1 - num2); break;
-            case "*": System.out.println(num1 * num2); break;
-            case "/": System.out.println(num1 / num2); break;
-
-            default: System.out.println("Math operator either does not exist or you did not input numbers correctly"); break;
+        if(health > 0) {
+            status = "Alive";
+        } else {
+            status = "Dead";
         }
+        System.out.println("What is your Status? " + status);
+        status = "";
 
-
-
-        int min = 1;
-        int max = 3;
-        String pickaxe = "Pickaxe";
-        String sword = "Sword";
-        String shovel = "Shovel";
-
-        int randomBlock = (int) (Math.random() * (max - min + 1)) + min;
-        System.out.println(randomBlock);
-
-        switch (randomBlock) {
-            case 1: System.out.println("Stone Block has appeared"); break;
-            case 2: System.out.println("Clay Block has appeared"); break;
-            case 3: System.out.println("Cobwebs has appeared"); break;
-        }
-
-
-
-
-        /* EXERCISE 1b: Basic Resource Miner Game
-         * Create a Basic "Resource Miner" Game. This is of course open to interpretation.
-         *
-         * The Basic idea is as follows: Via a random number generated, the "player" has one of (at least) three "blocks"
-         * in front of them. Let's call them Clay, Stone, Cobweb as examples. The player must then decide which tool to use
-         * Sword, Shovel, Pickaxe. Is the correct tool chosen then the player moves onto the next randomly selected block.
-         * They should mine three blocks and then the game outputs the score on how many they mined correctly!
-         *
-         * When the player types in their tool, make sure to do a small amount of error correction. Make sure that both
-         * lower and uppercase for the tool names are basically ignored! (Lesson: String Methods)
-         *
-         * For ease of use this, generates a number between 1 and 3, feel free to use it:
-         * int min = 1;
-         * int max = 3;
-         * int randomBlock = (int) (Math.random() * (max - min + 1)) + min;
-         *
-         * Estimated Time ~60+ minutes
-         *
-         */
+        // TERNARY OPERATOR
+        // BOOLEAN ? TRUE_RETURN : FALSE_RETURN;
+        status = (health > 0) ? "Alive" : "Dead";
+        System.out.println("What is your Status? " + status);
 
 
     }
