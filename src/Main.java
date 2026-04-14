58cb9d2170eb400e0d98a68621907a99da5ce9f2
tyron <wishsmp1002@gmail.com>
2026-04-14 14:15:42 +1000

    Lists, Sets, Maps, Stacks & Queues.

--- src/Main.java
+++ src/Main.java
@@ -1,64 +1,96 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        /* EXERCISE 2a: Easy Calculator (Now with Methods)
-         *
-         * Use the same Calculator, we've made in Exercise 1a and remake it with methods!
-         * In addition, make it so that the calculator asks if you'd like to continue, you can then say "y" if you do!
-         * Think how this "infinite" loop could be made! (Hint: Lesson Loops)
-         *
-         * Estimated Time: 30-45 minutes
-         *
-         */
-        caculate();
-    }
+        /* COLLECTIONS */
+        // For when an Array is just not enough!
 
-    public static void caculate() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Type in the operation (plus or +, minus or -, multiply or *, divide or /)");
-        String operation = scanner.next();
+        // Reminder Arrays... But what if we want to EXPAND?
+        String[] questions = new String[4];
+        questions[0] = "What does WWW stand for?";
+        questions[1] = "What is the World's largest Ocean?";
+        questions[2] = "Which Year did East and West Germany Unify?";
+        questions[3] = "What is the tallest mountain on Earth?";
 
-        System.out.println("Type in your first number!");
-        int number1 = scanner.nextInt();
-        System.out.println("Type in your second number!");
-        int number2 = scanner.nextInt();
+        String[] answers = new String[4];
+        answers[0] = "World Wide Web";
+        answers[1] = "Pacific Ocean";
+        answers[2] = "1990";
+        answers[3] = "Mt. Everest";
+
+        /* LIST */
+        // The much BETTER Array
+        // List of String
+        List<String> moreQuestions = new ArrayList<>(); // Diamond Operator <> for "Generic Types"
+
+        moreQuestions.add("What websites hosts the most videos in the world?");
+        moreQuestions.add("What is the Capital of Germany?");
+        System.out.println(moreQuestions.get(0));
 
 
-        int result = 0;
+        // Within the angle brackets we can put whatever data type we want that is not a basic one
+        // use Class equivalent aka the Uppercase Letter version
+        // List<Integer> numbers = new ArrayList<>();
+        // List<Boolean> truthValues = new ArrayList<>();
 
-        switch (operation) {
-            case "plus", "+":
-                result = number1 + number2; wantToContinue();
-                break;
-            case "minus", "-":
-                result = number1 - number2; wantToContinue();
-                break;
-            case "multiply", "*":
-                result = number1 * number2; wantToContinue();
-                break;
-            case "divide", "/": wantToContinue();
-                result = number1 / number2; break;
-            default: wantToContinue(); break;
-
+        // Another type of For Loop
+        // for each String question : whatever collection we want to loop through
+        // for each String question inside moreQuestions output them
+        for(String question : moreQuestions) {
+            System.out.println(question);
         }
-        System.out.println("Your result it " + number1 + " " + operation + " " + number2 + " = " + result);
-    }
 
-    public static void wantToContinue() {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Do you want to continue if so type \"y\"");
-        String calculatorContinue = scanner.next();
+        /* SET */
+        // Where each element appears once!
+        Set<String> countriesToVisit = new HashSet<>();
+        countriesToVisit.add("France");
+        countriesToVisit.add("Germany");
+        countriesToVisit.add("Spain");
+        countriesToVisit.add("France");
 
-        if (calculatorContinue.equals("y")) {
-            while (true) {
-                caculate();
-            }
-        } else {
-            System.out.println("Calculator Stopped");
-        }
+        System.out.println(countriesToVisit);
+
+        /* MAP */
+        // To Map one Thing to Another (key-Value Pair)
+        Map<String, Integer> countryToPopulationMap = new HashMap<>();
+        countryToPopulationMap.put("USA", 331000000);
+        countryToPopulationMap.put("United Kingdom", 67330000);
+        countryToPopulationMap.put("Austria", 8950000);
+
+        System.out.println(countryToPopulationMap.get("USA"));
+        System.out.println(countryToPopulationMap.get("Germany")); // gives us a null because does not exist
+        System.out.println(countryToPopulationMap); // outputting the whole Map
+
+        /* STACK & QUEUE */
+        // Stacks => LIFO (Last in, First Out)
+        // Queue => FIFO (First in, First Out)
+        Stack<String> cards = new Stack<>();
+        cards.add("King of Spades");
+        cards.add("Queen of Diamonds");
+        cards.add("Three of Clubs");
+
+        // Pop will get (AND REMOVE) the "top" element of the stack (Taking the card off of the deck)
+        System.out.println(cards.pop());
+        // Peek will look at the top element "get it" but not remove it (Look at card put it back again)
+        System.out.println(cards.peek());
+
+        Queue<String> registerQueue = new ArrayDeque<>();
+        registerQueue.add("Kaupenjoe");
+        registerQueue.add("PumpkinDev");
+        registerQueue.add("SantaEevee");
+        registerQueue.add("Magnet108");
+        registerQueue.add("Panda");
+
+        // Poll will get (AND REMOVE) the "front" element (in the case the first person at the register)
+        System.out.println(registerQueue.poll());
+        // Peek once again will get, but not remove the person at the front!
+        System.out.println(registerQueue.peek());
+
+
+
     }
 }
+
 
 
 
